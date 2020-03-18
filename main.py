c83b7f90568b88ef26db224ec1408229c8e379f4
Timothy O <timoliver200@gmail.com>
2020-03-18 13:46:32 -0500

    added import of time class should we wish to use its functions

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 #               Timothy Oliver
 # 
 # Project Summary: we make cool things yo
-#                   Yo, yo, yo! --I apologize for this test--
+#                   
 ########################################################################
 
 from tkinter import *
+import time
 
 ######################
 ######## Main ########
