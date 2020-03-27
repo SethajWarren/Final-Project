d3bafe8086851dee99e1f3724a2779efeeeb5521
Seth Warren <sethajwarren@gmail.com>
2020-03-27 09:23:07 -0500

    Coded a shuffle method. Added functions for everything I could think of
    need some ideas how to give the cards a suit, i have the ranking, etc planned out, just need to know which suit to display on screen.

--- main.py
+++ main.py
@@ -7,9 +7,70 @@
 #                   
 ########################################################################
 
-from tkinter import *
+from Tkinter import *
 import time
+from random import randint
+
 
 ######################
 ######## Main ########
 ######################
+
+class Battle(Canvas):
+    def __init__(self, master):
+        Canvas.__init__(self, master, bg = "olivedrab")
+        self.pack(fill = BOTH, expand = 1)
+
+    def mainScreen(self):
+        #main screen: options to play, go to store, equip modifiers: (eg. table backgrounds, deck colors)
+        pass
+    
+    def store(self):
+        #option to purchase in-game items, decks, backgrounds
+        pass
+
+    def options(self):
+        #settings screen, equip modifiers
+        pass
+
+    def save(self):
+        #save money and high scores, stuff unlocked, etc
+        pass
+
+    def bet(self):
+        #bet from your pool of money, calculates the money you get after each hand
+        pass
+
+    def shuffle(self):
+        #starts the game by shuffling and distributing cards
+        deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A"]
+        rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+        suits = ["spades", "clubs", "diamonds", "hearts"]
+        mydeck = []
+        otherdeck = []
+        for i in range(26):
+            print len(deck)
+            x = randint(0, len(deck)-1)
+            mydeck.append(deck[x])
+            del deck[x]
+            y = randint(0, len(deck)-1)
+            otherdeck.append(deck[y])
+            del deck[y]
+        return mydeck, otherdeck
+
+    def play():
+        mydeck, otherdeck = shuffle()
+        #runs the game
+        pass
+
+    
+        
+
+    
+WIDTH = 1080
+HEIGHT = 720
+window = Tk()
+window.geometry("{}x{}".format(WIDTH, HEIGHT))
+window.title("War")
+g = Battle(window)
+window.mainloop()
