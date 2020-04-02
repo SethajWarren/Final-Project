ad75092a9639698eeedb257f4560cd94db017c6a
Carter Ray <carterray31001@gmail.com>
2020-04-02 12:33:59 -0500

    working on card class
    need to figure out how to reference objects outside the function they were assigned to a varbile in

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ from random import randint
 ######################
 ######## Main ########
 ######################
+class Card(object):
+    val = {"s2":2, "c2":2, "d2":2, "h2":2, "s3":3, "c3":3, "d3":3, "h3":3, "s4":4, "c4":4, "d4":4, "h4":4, "s5":5, "c5":5, "d5":5, "h5":5, "s6":6, "c6":6, "d6":6, "h6":6, "s7":7, "c7":7, "d7":7, "h7":7, "s8":8, "c8":8, "d8":8, "h8":8, "s9":9, "c9":9, "d9":9, "h9":9, "s10":10, "c10":10, "d10":10, "h10":10, "sJ":11, "cJ":11, "dJ":11, "hJ":11, "sQ":12, "cQ":12, "dQ":12, "hQ":12, "sK":13, "cK":13, "dK":13, "hK":13, "sA":14, "cA":14, "dA":14, "hA":14}
+    def __init__(self, name, image = None):
+        self.name = name
+        self.image = image
+        self.value = Card.val[name]
+    
+    def __str__(self):
+        return self.name
+        
 
 class Battle(Canvas):
     def __init__(self, master):
@@ -41,10 +51,64 @@ class Battle(Canvas):
         #bet from your pool of money, calculates the money you get after each hand
         pass
 
+    def setupCards(self):
+        s2 = Card("s2")
+        c2 = Card("c2")
+        d2 = Card("d2")
+        h2 = Card("h2")
+        s3 = Card("s3")
+        c3 = Card("c3")
+        d3 = Card("d3")
+        h3 = Card("h3")
+        s4 = Card("s4")
+        c4 = Card("c4")
+        d4 = Card("d4")
+        h4 = Card("h4")
+        s5 = Card("s5")
+        c5 = Card("c5")
+        d5 = Card("d5")
+        h5 = Card("h5")
+        s6 = Card("s6")
+        c6 = Card("c6")
+        d6 = Card("s6")
+        h6 = Card("h6")
+        s7 = Card("s7")
+        c7 = Card("c7")
+        d7 = Card("d7")
+        h7 = Card("h7")
+        s8 = Card("s8")
+        c8 = Card("c8")
+        d8 = Card("d8")
+        h8 = Card("h8")
+        s9 = Card("s9")
+        c9 = Card("c9")
+        d9 = Card("d9")
+        h9 = Card("h9")
+        s10 = Card("s10")
+        c10 = Card("c10")
+        d10 = Card("d10")
+        h10 = Card("h10")
+        sJ = Card("sJ")
+        cJ = Card("cJ")
+        dJ = Card("dJ")
+        hJ = Card("hJ")
+        sQ = Card("sQ")
+        cQ = Card("cQ")
+        dQ = Card("dQ")
+        hQ = Card("hQ")
+        sK = Card("sK")
+        cK = Card("cK")
+        dK = Card("dK")
+        hK = Card("hK")
+        sA = Card("sA")
+        cA = Card("cA")
+        dA = Card("dA")
+        hA = Card("hA")
+
+        Battle.myCard
+
     def shuffle(self):
         #starts the game by shuffling and distributing cards
-        deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A"]
-        rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
         suits = ["spades", "clubs", "diamonds", "hearts"]
         mydeck = []
         otherdeck = []
@@ -56,12 +120,17 @@ class Battle(Canvas):
             otherdeck.append(deck[y])
             del deck[y]
         print deck
+        print
+        print mydeck
+        print
+        print otherdeck
         return mydeck, otherdeck
 
     def play():
         mydeck, otherdeck = shuffle()
         #runs the game
         pass
+    
 
     
     
@@ -71,4 +140,6 @@ window = Tk()
 window.geometry("{}x{}".format(WIDTH, HEIGHT))
 window.title("War")
 g = Battle(window)
-window.mainloop()
+g.setupCards()
+print hA
+#window.mainloop()
